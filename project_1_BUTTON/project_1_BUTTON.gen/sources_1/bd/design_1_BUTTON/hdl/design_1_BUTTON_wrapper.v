87f184326fbd01884304c740ccdf75b92262b391
Appletea <dongs4739@gmail.com>
2026-04-03 15:46:33 +0900

    Button

---
+++ project_1_BUTTON/project_1_BUTTON.gen/sources_1/bd/design_1_BUTTON/hdl/design_1_BUTTON_wrapper.v
@@ -0,0 +1,41 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2024.2 (lin64) Build 5239630 Fri Nov 08 22:34:34 MST 2024
+//Date        : Fri Apr  3 11:28:01 2026
+//Host        : Appletea running 64-bit Ubuntu 24.04.4 LTS
+//Command     : generate_target design_1_BUTTON_wrapper.bd
+//Design      : design_1_BUTTON_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_BUTTON_wrapper
+   (led_16bits_tri_o,
+    push_buttons_4bits_tri_i,
+    reset,
+    sys_clock,
+    usb_uart_rxd,
+    usb_uart_txd);
+  output [15:0]led_16bits_tri_o;
+  input [3:0]push_buttons_4bits_tri_i;
+  input reset;
+  input sys_clock;
+  input usb_uart_rxd;
+  output usb_uart_txd;
+
+  wire [15:0]led_16bits_tri_o;
+  wire [3:0]push_buttons_4bits_tri_i;
+  wire reset;
+  wire sys_clock;
+  wire usb_uart_rxd;
+  wire usb_uart_txd;
+
+  design_1_BUTTON design_1_BUTTON_i
+       (.led_16bits_tri_o(led_16bits_tri_o),
+        .push_buttons_4bits_tri_i(push_buttons_4bits_tri_i),
+        .reset(reset),
+        .sys_clock(sys_clock),
+        .usb_uart_rxd(usb_uart_rxd),
+        .usb_uart_txd(usb_uart_txd));
+endmodule
